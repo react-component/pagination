c429690432d8543732bfc2cd1c1770cfd68a3652
Yuwei Ba <i@xiaoba.me>
2016-03-01 20:00:29 +0800

    spec

---
+++ tests/helper/two-pagination.jsx
@@ -0,0 +1,30 @@
+require('rc-select/assets/index.css');
+const Pagination = require('rc-pagination');
+const React = require('react');
+import Select from 'rc-select';
+
+const Hello = React.createClass({
+  getInitialState: function() {
+    return {pageSize: 20};
+  },
+  changeSize() {
+    this.setState({
+      pageSize: 50,
+    });
+  },
+  render() {
+    return (<div style={{margin: 10}}>
+      <button className="hook" onClick={this.changeSize}>ChaneSize</button>
+      <Pagination
+        className="p1"
+        selectComponentClass={Select}
+        showSizeChanger pageSize={this.state.pageSize} onShowSizeChange={this.onShowSizeChange} defaultCurrent={3} total={500} />
+      <Pagination
+        className="p2"
+        selectComponentClass={Select}
+        showSizeChanger pageSize={this.state.pageSize} onShowSizeChange={this.onShowSizeChange} defaultCurrent={3} total={500} />
+    </div>);
+  },
+});
+
+export default Hello;

3f6e8824f4c18b832aedcc8da7cf6e8ae75dcb95
Carl Gödecken <carl.goedecken@student.hpi.de>
2017-01-19 14:59:15 +0100

    Add German locale

---
+++ src/locale/de_DE.js
@@ -0,0 +1,12 @@
+export default {
+  // Options.jsx
+  items_per_page: 'pro Seite',
+  jump_to: 'Gehe zu',
+  page: 'Seite',
+
+  // Pagination.jsx
+  prev_page: 'Vorherige Seite',
+  next_page: 'Nächste Seite',
+  prev_5: 'Vorherige 5 Seiten',
+  next_5: 'Nächste 5 Seiten',
+};

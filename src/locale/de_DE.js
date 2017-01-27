81ec4ff9e12ec52a5e2c42f1e6bba18fbfb78d99
Simon Waloschek <sonovice@users.noreply.github.com>
2017-01-27 11:05:55 +0100

    Translated

--- src/locale/de_DE.js
+++ src/locale/de_DE.js
@@ -1,12 +1,12 @@
 export default {
   // Options.jsx
-  items_per_page: '/ page',
-  jump_to: 'Goto',
+  items_per_page: '/ Seite',
+  jump_to: 'Gehe zu',
   page: '',
 
   // Pagination.jsx
-  prev_page: 'Previous Page',
-  next_page: 'Next Page',
-  prev_5: 'Previous 5 Pages',
-  next_5: 'Next 5 Pages',
+  prev_page: 'Vorherige Seite',
+  next_page: 'Nächste Seite',
+  prev_5: '5 Seiten zurück',
+  next_5: '5 Seiten vor',
 };

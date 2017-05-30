817168fa75a54baf106acf191ea78bc27e08f0f3
Damian Green <damian.green@microlease.com>
2017-05-30 09:39:01 +0100

    add en-GB / it-IT / nl-BE locales

---
+++ src/locale/it_IT.js
@@ -0,0 +1,14 @@
+export default {
+  // Options.jsx
+  items_per_page: '/ pagina',
+  jump_to: 'vai a',
+  page: '',
+
+  // Pagination.jsx
+  prev_page: 'Pagina precedente',
+  next_page: 'Pagina successiva',
+  prev_5: 'Precedenti 5 pagine',
+  next_5: 'Prossimi 5 pagine',
+  prev_3: 'Precedenti 3 pagine',
+  next_3: 'Prossimi 3 pagine',
+};

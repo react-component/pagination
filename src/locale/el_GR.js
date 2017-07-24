d881a40f41fb7934df380aa7c6ae091a3b71f46c
Michalis Macheras <diodosier@gmail.com>
2017-07-24 05:17:12 +0300

    feat: add el_GR locale (Greek) (#96)

---
+++ src/locale/el_GR.js
@@ -0,0 +1,14 @@
+export default {
+  // Options.jsx
+  items_per_page: '/ σελίδα',
+  jump_to: 'Μετάβαση',
+  page: '',
+
+  // Pagination.jsx
+  prev_page: 'Προηγούμενη Σελίδα',
+  next_page: 'Επόμενη Σελίδα',
+  prev_5: 'Προηγούμενες 5 Σελίδες',
+  next_5: 'Επόμενες 5 σελίδες',
+  prev_3: 'Προηγούμενες 3 Σελίδες',
+  next_3: 'Επόμενες 3 Σελίδες',
+};

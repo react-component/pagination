0882f6d9642eb36cd305fa809979baf2c5656c77
Abdulhalim Kara <ahalimkara@gmail.com>
2018-01-18 10:22:30 +0300

    Create ku_IQ.js
    Kurdish translation for Kurdistan Region in Iraq.

---
+++ src/locale/ku_IQ.js
@@ -0,0 +1,15 @@
+export default {
+  // Options.jsx
+  items_per_page: '/ rûpel',
+  jump_to: 'Biçe',
+  jump_to_confirm: 'piştrast bike',
+  page: '',
+
+  // Pagination.jsx
+  prev_page: 'Rûpelê Berê',
+  next_page: 'Rûpelê Din',
+  prev_5: '5 Rûpelên Berê',
+  next_5: '5 Rûpelên Din',
+  prev_3: '3 Rûpelên Berê',
+  next_3: '3 Rûpelên Din',
+};

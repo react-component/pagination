98014ca422a17aebfff6ad44eb36d1445a2e5fc0
digz6666 <digz6666@gmail.com>
2018-09-03 19:35:46 +0800

    Added Mongolian locale

---
+++ src/locale/mn_MN.js
@@ -0,0 +1,15 @@
+export default {
+  // Options.jsx
+  items_per_page: '/ хуудас',
+  jump_to: 'Шилжих',
+  jump_to_confirm: 'сонгох',
+  page: '',
+
+  // Pagination.jsx
+  prev_page: 'Өмнөх хуудас',
+  next_page: 'Дараа хуудас',
+  prev_5: 'Өмнөх 5 хуудас',
+  next_5: 'Дараа 5 хуудас',
+  prev_3: 'Өмнөх 3 хуудас',
+  next_3: 'Дараа 3 хуудас',
+};

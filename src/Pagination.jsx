6eb6b1b56a8b0b883fe1b0048a230aee6ff03b7a
xrkffgg <xrkffgg@vip.qq.com>
2021-03-15 16:32:59 +0800

    fix api

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -90,11 +90,6 @@ class Pagination extends React.Component {
       if (lastCurrentNode && document.activeElement === lastCurrentNode) {
         lastCurrentNode.blur();
       }
-    } else if (this.paginationNode) {
-      const activeNode = this.paginationNode.querySelector(
-        `.${prefixCls}-item-active`,
-      );
-      activeNode?.focus();
     }
   }
 

99e55d7496746de79102d2062b8d98069d3dad76
WingRay Liao <rayliao1989@gmail.com>
2021-04-01 16:53:31 +0800

    Create km_KH.js

---
+++ src/locale/km_KH.js
@@ -0,0 +1,15 @@
+export default {
+  // Options.jsx
+  items_per_page: 'អត្ថបទ/ទំព័រ',
+  jump_to: 'លោត​ទៅ',
+  jump_to_confirm: 'កំណត់',
+  page: 'ទំព័រ',
+
+  // Pagination.jsx
+  prev_page: 'ទំព័រ​មុន',
+  next_page: 'ទំ​ព​រ័​បន្ទាប់',
+  prev_5: '៥ ទំព័រទៅមុខ',
+  next_5: '៥ ទំព័រថយក្រោយ',
+  prev_3: '3 ទំព័រទៅមុខ',
+  next_3: '៣ ទំព័រត្រឡប់មកវិញ',
+};

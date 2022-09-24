4f32e6b7276a7e5a52951ed6ba6fc0f8578d5a26
Zablon Dawit <zabjd22@gmail.com>
2022-09-24 21:37:40 +0300

    fix: syntax error, missing semi-colon

--- src/locale/am.js
+++ src/locale/am.js
@@ -8,9 +8,9 @@ export default {
     // Pagination.jsx
     prev_page: 'ያለፈው ገፅ',
     next_page: 'ቀጣይ ገፅ',
-    prev_5: 'ያለፈው 5 ገፅ'
+    prev_5: 'ያለፈው 5 ገፅ',
     next_5: 'ቀጣይ 5 ገፅ',
-    prev_3: 'ያለፈው 3 ገፅ'
+    prev_3: 'ያለፈው 3 ገፅ',
     next_3: 'ቀጣይ 3 ገፅ',
     page_size: 'የገፅ መጠን',
 };

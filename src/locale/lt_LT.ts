4e658c13184e4761a10d04db9639d035ad7d2ed3
Digital-512 <57298727+Digital-512@users.noreply.github.com>
2023-05-26 22:55:41 +0300

    Update lt_LT.ts
    What has been changed:
    - Minor improvements
    - Translated "Page Size" -> "Puslapio dydis"
    - Fix typo: "pls." is incorrect, it should be "psl."

--- src/locale/lt_LT.ts
+++ src/locale/lt_LT.ts
@@ -1,16 +1,16 @@
 export default {
   // Options.jsx
   items_per_page: '/ psl.',
-  jump_to: 'Pereiti',
+  jump_to: 'Pereiti į',
   jump_to_confirm: 'patvirtinti',
-  page: '',
+  page: 'psl.',
 
   // Pagination.jsx
   prev_page: 'Atgal',
   next_page: 'Pirmyn',
-  prev_5: 'Grįžti 5 pls.',
-  next_5: 'Peršokti 5 pls.',
-  prev_3: 'Grįžti 3 pls.',
-  next_3: 'Peršokti 3 pls.',
-  page_size: 'Page Size',
+  prev_5: 'Grįžti 5 psl.',
+  next_5: 'Peršokti 5 psl.',
+  prev_3: 'Grįžti 3 psl.',
+  next_3: 'Peršokti 3 psl.',
+  page_size: 'Puslapio dydis',
 };

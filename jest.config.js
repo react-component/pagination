caabba686b08ed5fef5a21fe0c4eb95373bfeb08
wuxh <wxh1220@gmail.com>
2023-11-04 01:37:32 +0800

    test: add example test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
+const pkg = require('./package.json');
 module.exports = {
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
+  moduleNameMapper: {
+    [pkg.name]: '<rootDir>/src/index.ts',
+    '\\.less$': 'identity-obj-proxy',
+  },
 };

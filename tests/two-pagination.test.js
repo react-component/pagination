87a20d40e8a75fbb5dadaa78f9ff5e4a60ecdd75
wuxh <wxh1220@gmail.com>
2023-11-20 03:03:50 +0800

    test: update test case

--- tests/two-pagination.test.js
+++ tests/two-pagination.test.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { mount } from 'enzyme';
+import React from 'react';
 import Pagination from '../src';
 import TwoPagination from './two-pagination';
 
@@ -17,15 +17,22 @@ describe('Two Pagination', () => {
   it('should has initial pageSize 20', () => {
     const p1 = wrapper.find(Pagination).at(0);
     const p2 = wrapper.find(Pagination).at(1);
-    expect(p1.state().pageSize).toBe(20);
-    expect(p2.state().pageSize).toBe(20);
+    // expect(p1.state().pageSize).toBe(20); // Class component
+    expect(p1.props().pageSize).toBe(20); // Function component
+    // expect(p2.state().pageSize).toBe(20); // Class component
+    expect(p2.props().pageSize).toBe(20); // Function component
   });
 
   it('should sync pageSize via state', () => {
     const p1 = wrapper.find(Pagination).at(0);
     const p2 = wrapper.find(Pagination).at(1);
     wrapper.find('.hook').simulate('click');
-    expect(p1.state().pageSize).toBe(50);
-    expect(p2.state().pageSize).toBe(50);
+    // wrapper.update();
+    const newP1 = wrapper.find(Pagination).at(0);
+    const newP2 = wrapper.find(Pagination).at(1);
+    // expect(p1.state().pageSize).toBe(50); // Class component
+    expect(newP1.props().pageSize).toBe(50); // Function component
+    // expect(p2.state().pageSize).toBe(50); // Class component
+    expect(newP2.props().pageSize).toBe(50); // Function component
   });
 });

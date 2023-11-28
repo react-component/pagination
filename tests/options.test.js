8e7e387a2371c486afed423a7246c0d4b8146ab3
wuxh <wxh1220@gmail.com>
2023-11-28 10:19:19 +0800

    test: add case

---
+++ tests/options.test.js
@@ -0,0 +1,41 @@
+import { mount } from 'enzyme';
+import Select from 'rc-select';
+import zhCN from '../src/locale/zh_CN';
+import Options from '../src/Options';
+
+const WrapperOptions = (props) => {
+  return (
+    <Options
+      locale={zhCN}
+      rootPrefixCls="rc-pagination"
+      selectComponentClass={Select}
+      pageSize={10}
+      changeSize={jest.fn()}
+      quickGo={jest.fn()}
+      {...props}
+    />
+  );
+};
+
+describe('Options', () => {
+  it('should render correctly', () => {
+    const wrapper = mount(<WrapperOptions />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  describe('props:buildOptionText', () => {
+    it('should render correctly', () => {
+      const mockBuildOptionText = jest
+        .fn()
+        .mockImplementation((value) => (
+          <div className="custom-options">buildOptionText-{value}</div>
+        ));
+      const wrapper = mount(
+        <WrapperOptions buildOptionText={mockBuildOptionText} />,
+      );
+      const options = wrapper.find('.custom-options');
+      expect(options).toBeTruthy();
+      expect(options.text()).toBe('buildOptionText-10');
+    });
+  });
+});

469715550865809a9870de1f88f8ea408ac52c5d
MadCcc <1075746765@qq.com>
2023-12-11 16:06:55 +0800

    fix: input value should follow current

--- docs/examples/simple.tsx
+++ docs/examples/simple.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pagination from 'rc-pagination';
 import Select from 'rc-select';
 import '../../assets/index.less';
 
-export default () => (
-  <>
-    <Pagination simple defaultCurrent={1} total={50} />
-    <br />
-    <Pagination
-      simple
-      defaultCurrent={1}
-      total={50}
-      showTotal={(total) => `Total ${total} items`}
-    />
-    <br />
-    <Pagination
-      simple
-      defaultCurrent={1}
-      total={50}
-      showSizeChanger
-      selectComponentClass={Select}
-    />
-  </>
-);
+export default () => {
+  const [pageIndex, setPageIndex] = useState(1);
+
+  return (
+    <>
+      <button onClick={() => setPageIndex((i) => ++i)}>increase</button>
+      <Pagination
+        simple
+        current={pageIndex}
+        total={50}
+        onChange={setPageIndex}
+      />
+      <br />
+      <Pagination simple defaultCurrent={1} total={50} />
+      <br />
+      <Pagination
+        simple
+        defaultCurrent={1}
+        total={50}
+        showTotal={(total) => `Total ${total} items`}
+      />
+      <br />
+      <Pagination
+        simple
+        defaultCurrent={1}
+        total={50}
+        showSizeChanger
+        selectComponentClass={Select}
+      />
+    </>
+  );
+};

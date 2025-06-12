5f897f666296649b03caf8a80681a667f70d83b9
Radha Rani <142741446+radha-rani-n@users.noreply.github.com>
2025-06-11 23:29:12 -0400

    add telugu language support (#656)

---
+++ src/locale/te_IN.ts
@@ -0,0 +1,20 @@
+import type { PaginationLocale } from '../interface';
+
+const locale: PaginationLocale = {
+  //options
+  items_per_page: '/ పేజీ',
+  jump_to: 'వెళ్ళండి',
+  jump_to_confirm: 'నిర్ధారించండి',
+  page: 'పేజీ',
+
+  //pagination
+  prev_page: 'మునుపటి పేజీ',
+  next_page: 'తదుపరి పేజీ',
+  prev_5: 'మునుపటి 5 పేజీలు',
+  next_5: 'తదుపరి 5 పేజీలు',
+  prev_3: 'మునుపటి 3 పేజీలు',
+  next_3: 'తదుపరి 3 పేజీలు',
+  page_size: 'పేజీ పరిమాణం',
+};
+
+export default locale;

270ddcc901a946de84bc31ee7f2044c8f384fc70
divyeshagrawal <138372682+divyeshagrawal@users.noreply.github.com>
2025-09-26 08:21:45 +0530

    feat: Add Marathi language support (#658)
    * feat: Add Marathi language support
    
    * feat: update translations

---
+++ src/locale/mr_IN.ts
@@ -0,0 +1,20 @@
+import type { PaginationLocale } from '../interface';
+
+const locale: PaginationLocale = {
+  // Options
+  items_per_page: '/ पृष्ठ',
+  jump_to: 'यावर जा ',
+  jump_to_confirm: 'पुष्टी करा',
+  page: 'पृष्ठ',
+
+  // Pagination
+  prev_page: 'मागील पृष्ठ',
+  next_page: 'पुढील पृष्ठ',
+  prev_5: 'मागील ५ पृष्ठे',
+  next_5: 'पुढील ५ पृष्ठे',
+  prev_3: 'मागील ३ पृष्ठे',
+  next_3: 'पुढील ३ पृष्ठे',
+  page_size: 'पृष्ठ आकार',
+};
+
+export default locale;

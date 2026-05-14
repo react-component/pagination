565edeb34d6fe48140568681970c92eca6d50c63
Ronin <roninxincu@gmail.com>
2026-05-14 14:07:50 +0800

    feat: add Filipino (fil_PH) locale for pagination

---
+++ src/locale/fil_PH.ts
@@ -0,0 +1,20 @@
+import type { PaginationLocale } from '../interface';
+
+const locale: PaginationLocale = {
+  // Options
+  items_per_page: '/ pahina',
+  jump_to: 'Pumunta sa',
+  jump_to_confirm: 'kumpirmahin',
+  page: 'Pahina',
+
+  // Pagination
+  prev_page: 'Nakaraang Pahina',
+  next_page: 'Susunod na Pahina',
+  prev_5: 'Nakaraang 5 Pahina',
+  next_5: 'Susunod na 5 Pahina',
+  prev_3: 'Nakaraang 3 Pahina',
+  next_3: 'Susunod na 3 Pahina',
+  page_size: 'Sukat ng Pahina',
+};
+
+export default locale;
